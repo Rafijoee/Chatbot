686d14eb22b344dbd6a883c73978c5bc9726c3f9
Rafijoee <rafijoe44@gmail.com>
2025-04-26 23:08:52 +0700

    feat : function authenticate

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+    
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->intended('dashboard');
+    }
+}
