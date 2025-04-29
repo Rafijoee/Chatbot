163a3f93abecc63b69e48ea7c47396eb62223ec3
Rafijoee <rafijoe44@gmail.com>
2025-04-29 23:32:41 +0700

    feat : chat model

---
+++ database/migrations/2025_04_28_183333_create_chats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('message');
+            $table->foreignId('to_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('is_read')->default(false);
+            $table->boolean('is_deleted')->default(false);
+            $table->boolean('is_deleted_by_to')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
